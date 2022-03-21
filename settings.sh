0cc81e42250e367ded12c7747d3f1f2ac6d94f44
evanparizot <eparizot@gmail.com>
2022-03-21 19:23:17 -0400

    Copy fish config

--- settings.sh
+++ settings.sh
@@ -4,4 +4,4 @@
 
 cp init.vim $HOME/.config/nvim/init.vim && echo "Copied neovim settings"
 cp .gitconfig $HOME/.gitconfig && echo "Copied git config settings"
-
+cp config.fish $HOME/.config/fish/ && echo "Copied fish config settings"
